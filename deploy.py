d46ba8ee0f98e4429cef5f10eee22c7cea1a1b93
nekoside <qwqnekoo@gmail.com>
2026-01-23 23:12:12 +0800

    modified:   deploy.py

--- deploy.py
+++ deploy.py
@@ -13,7 +13,7 @@ print('开始构建')
 run('pnpm build')
 
 print('添加构建产物到 stash')
-run('git add /docs/.vitepress/dist/ -f')
+run('git add docs/.vitepress/dist/ -f')
 # run('git stash push -m "Update"')
 
 print('切换到 deploy 分支')
@@ -23,7 +23,7 @@ print('拉取远程仓库')
 run('git pull')
 
 print('清空 deploy 分支')
-run('git rm -rf .')
+# run('git rm -rf ')
 run('git clean -fdx')
 # run('git stash pop stash@{0}')
 # run('git add .')
@@ -32,7 +32,7 @@ print('提交 stash')
 run('git commit -m "Update"')
 
 print('push 到远程仓库')
-run('git push')
+run('git push deploy')
 
 print('切换到 main 分支')
 run('git checkout main')
