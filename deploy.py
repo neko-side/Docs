a740384f68e08d6839ef0c41110c313d8656512d
nekoside <qwqnekoo@gmail.com>
2026-01-23 23:06:44 +0800

    modified:   deploy.py

--- deploy.py
+++ deploy.py
@@ -18,11 +18,21 @@ run('git add /docs/.vitepress/dist/ -f')
 
 print('切换到 deploy 分支')
 run('git checkout deploy')
+
+print('拉取远程仓库')
 run('git pull')
+
+print('清空 deploy 分支')
 run('git rm -rf .')
 run('git clean -fdx')
-run('git stash pop stash@{0}')
-run('git add .')
+# run('git stash pop stash@{0}')
+# run('git add .')
+
+print('提交 stash')
 run('git commit -m "Update"')
+
+print('push 到远程仓库')
 run('git push')
+
+print('切换到 main 分支')
 run('git checkout main')
