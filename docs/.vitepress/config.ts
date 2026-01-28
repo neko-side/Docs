c073e71878fe5ac2118c16a889dc3c15706f097d
nekoside <qwqnekoo@gmail.com>
2026-01-28 16:09:32 +0800

    更新更新更新更新

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vitepress'
 
-// https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
     title: "nekoside",
     description: "",
-    base: "/docs/"
+    base: "/docs/",
+    themeConfig: {
+        siteTitle: false,
+        nav: [
+            {
+                text: '博客',
+                link: 'https://nekoside.com/',
+                target: '_self'
+            },
+            {
+                text: '文档',
+                link: 'https://nekoside.com/docs/',
+                target: '_self'
+            }
+        ]
+    }
 })
